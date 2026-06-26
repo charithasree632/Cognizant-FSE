be868e8f09642d9eef747615356235bdca6376a8
chari <charithasreeperam@gmail.com>
2026-06-26 17:35:33 +0530

    gitignore file

---
+++ src/com/singleton/TestSingleton.java
@@ -0,0 +1,11 @@
+package com.singleton;
+
+public class TestSingleton {
+    public static void main(String[] args) {
+        Logger logger1 = Logger.getInstance();
+        logger1.log("First message");
+        Logger logger2 = Logger.getInstance();
+        logger2.log("Second message");
+        System.out.println("Are both objects same? " + (logger1 == logger2));
+    }
+}
