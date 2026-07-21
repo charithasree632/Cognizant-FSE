0d9b2c92da22308b460461b2f3b0210768a84299
charithasree632 <charithasreeperam@gmail.com>
2026-07-22 01:34:33 +0530

    Setting Up JUnit

---
+++ TDD using JUnit5/test/java/com/example/JUnitDemo1/CalculatorTest.java
@@ -0,0 +1,29 @@
+package com.example.JUnitDemo1;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+
+    Calculator calculator = new Calculator();
+
+    @Test
+    public void testAdd() {
+        assertEquals(5, calculator.add(2, 3));
+    }
+
+    @Test
+    public void testSubtract() {
+        assertEquals(2, calculator.subtract(5, 3));
+    }
+
+    @Test
+    public void testMultiply() {
+        assertEquals(6, calculator.multiply(2, 3));
+    }
+
+    @Test
+    public void testDivide() {
+        assertEquals(2, calculator.divide(6, 3));
+    }
+}
