83d40ab2f62ba58d0657c96ef1d9bcfaced5e416
charithasree632 <charithasreeperam@gmail.com>
2026-07-28 08:42:49 +0530

    Configuring a Basic Spring Application

---
+++ Spring Core_Maven/java/com/library/main/LibraryApplication.java
@@ -0,0 +1,21 @@
+package com.library.main;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.library.service.BookService;
+
+public class LibraryApplication {
+
+    public static void main(String[] args) {
+
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        BookService service =
+                context.getBean("bookService", BookService.class);
+
+        service.showService();
+
+        context.close();   // Close the Spring context
+    }
+}
